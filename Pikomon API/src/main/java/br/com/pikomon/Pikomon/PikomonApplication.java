d07b4091628aa772fd41783a5480e59e86176dc9
Marcus Lira <marcus_lira2@hotmail.com>
2023-07-01 09:55:33 -0300

    first commit

---
+++ Pikomon API/src/main/java/br/com/pikomon/Pikomon/PikomonApplication.java
@@ -0,0 +1,13 @@
+package br.com.pikomon.Pikomon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PikomonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PikomonApplication.class, args);
+	}
+
+}
---
+++ Pikomon API/src/test/java/br/com/pikomon/Pikomon/PikomonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pikomon.Pikomon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PikomonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
